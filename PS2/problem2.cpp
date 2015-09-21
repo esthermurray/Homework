d0969f0ea2df848fbc83c8046a6c68b887ed7f4a
esthermurray <emurray3@illinois.edu>
2015-09-20 23:12:07 -0500

    PSET submission

--- PS2/problem2.cpp
+++ PS2/problem2.cpp
@@ -40,14 +40,16 @@ http://www.cplusplus.com/reference/cmath/pow/
 ************************************************
 */
 
-#include <iostream>
+#include <iostream> 
+#include <cmath>
 
 using namespace std;
 
+
 int main() {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 3; j > 0; j--) {
-            cout << pow(i, j) << endl;
-        }
-    }
+	for (int i = 0; i < 3; i++) {
+		for (int j = 3; j > 0; j--) {
+			cout << pow(i, j) << endl;
+		}
+	}
 }
--- PS2/problem4.java
+++ PS2/problem4.java
@@ -37,31 +37,33 @@ This sentence should print 4 times.
 ************************************************
 */
 
-class Problem4 {
-    public static void main(String[] args) {
-        printFirstSentence();
-        printSecondSentence();
-        printThirdSentence();
-    }
-
-    public static void printFirstSentence() {
-        for(int i = 5; i > 0; i++) {
-            System.out.println("This sentence should print 5 times.");
-        }
-    }
-
-    public static void printSecondSentence() {
-        int i = 3;
-        while(i > 0) {
-            System.out.println("This sentence should print 3 times.");
-            i++;
-        }
-    }
-
-    public static void printThirdSentence() {
-        for(int amount = 0; amount < 12; amount++) {
-            System.out.println("This sentence should print 4 times.")
-            amount--;
-        }
-    }
-}
+class Problem4 { 
+     public static void main(String[] args) { 
+         printFirstSentence(); 
+         printSecondSentence(); 
+         printThirdSentence(); 
+     } 
+ 
+ 
+     public static void printFirstSentence() { 
+         for(int i = 5; i > 0; i--) { 
+             System.out.println("This sentence should print 5 times."); 
+         } 
+     } 
+ 
+ 
+     public static void printSecondSentence() { 
+         int i = 3; 
+         while(i > 0) { 
+             System.out.println("This sentence should print 3 times."); 
+             i--; 
+         } 
+     } 
+ 
+     public static void printThirdSentence() { 
+         for(int amount = 0; amount < 4; amount++) { 
+             System.out.println("This sentence should print 4 times."); 
+             //amount--; 
+         } 
+     } 
+ } 
